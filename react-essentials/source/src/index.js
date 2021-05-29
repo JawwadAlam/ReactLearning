b6b7eaee45d83077a65f4f3a8d211c1c9bd33e41
JawwadAlam <jawwadalam@gmail.com>
2021-05-29 16:17:43 +0500

    Code changes for useState hook learning

--- react-essentials/source/src/index.js
+++ react-essentials/source/src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
+function AppWithStateExample(){
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
+  return <>
+    <h1>Current emotion is {emotion}</h1>
+    <button onClick={() => setEmotion("happy")}>happy</button>
+    <button onClick={() => setEmotion("frustrated")}>frustrate</button>
+    <button onClick={() => setEmotion("enthusiastic")}>enthuse</button>
+  </>
+}
+
 ReactDOM.render(
-  <App authorized={true}/>,
+  // <App authorized={true}/>,
+  <AppWithStateExample />,
   document.getElementById('root')
 );
