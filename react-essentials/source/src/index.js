37b71ef7ce4a0ae202c969ffd402107fd04e5fb8
JawwadAlam <jawwadalam@gmail.com>
2021-05-29 18:19:27 +0500

    using individual state elements

--- react-essentials/source/src/index.js
+++ react-essentials/source/src/index.js
@@ -46,13 +46,21 @@ function AppWithUseEffectForDataHook({login}) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
+console.log("I am being called")
+
     fetch(`https://api.github.com/users/${login}`)
     .then((response) => response.json())
     .then(setData);
   }, []);
 
-  if (login) {
-    return <div>{JSON.stringify(data)}</div>
+  if (data) {
+    return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>Total Repos: {data.public_repos}</p>
+      <img alt={data.login} src={data.avatar_url} />
+    </div>
+    );
   }
 
   return <div>No Data available</div>
