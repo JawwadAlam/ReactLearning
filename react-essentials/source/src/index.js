4c23b9665405035678815a3a065f35c529c06099
JawwadAlam <jawwadalam@gmail.com>
2021-05-29 16:41:50 +0500

    Use of useEffect hook

--- react-essentials/source/src/index.js
+++ react-essentials/source/src/index.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
+//import App from './App'
 
 function AppWithStateExample(){
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here`)
+  },[emotion]);
+
+  useEffect(() => {
+    console.log(`Secondary emotion is ${secondary}`);
+  }, [secondary]);
+
   return <>
-    <h1>Current emotion is {emotion}</h1>
+    <h1>Current emotion is {emotion} and {secondary}</h1>
     <button onClick={() => setEmotion("happy")}>happy</button>
-    <button onClick={() => setEmotion("frustrated")}>frustrate</button>
+    <button onClick={() => setSecondary("flustered")}>Make flustered</button>
+    <button onClick={() => setEmotion("frustrated")}>frustrate me</button>
+    <button onClick={() => setSecondary("confused")}>Confuse Me</button>
     <button onClick={() => setEmotion("enthusiastic")}>enthuse</button>
+    <button onClick={() => setSecondary("dizzy")}>Make Sleepy</button>
   </>
 }
 
