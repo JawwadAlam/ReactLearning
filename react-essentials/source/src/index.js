1dd75574a7577b9e5e0c0c68d7807541d1dbabfa
JawwadAlam <jawwadalam@gmail.com>
2021-05-29 17:00:19 +0500

    useReducer hook related code changes - can be used to toggle the state, instead of using long path of useState

--- react-essentials/source/src/index.js
+++ react-essentials/source/src/index.js
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App'
 
-function AppWithStateExample(){
+function AppWithStateExample() {
   const [emotion, setEmotion] = useState("happy");
   const [secondary, setSecondary] = useState("tired");
+  // const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+    );
 
   useEffect(() => {
     console.log(`It's ${emotion} around here`)
-  },[emotion]);
+  }, [emotion]);
 
   useEffect(() => {
     console.log(`Secondary emotion is ${secondary}`);
   }, [secondary]);
+
+  // function toggle(){
+  //   setChecked((checked) => !checked)
+  // }
 
   return <>
     <h1>Current emotion is {emotion} and {secondary}</h1>
@@ -23,6 +32,10 @@ function AppWithStateExample(){
     <button onClick={() => setSecondary("confused")}>Confuse Me</button>
     <button onClick={() => setEmotion("enthusiastic")}>enthuse</button>
     <button onClick={() => setSecondary("dizzy")}>Make Sleepy</button>
+
+    {/* <input type="checkbox" onChange={() => setChecked((checked) => !checked)} value={checked} /> */}
+    <input type="checkbox" onChange={toggle} value={checked} />
+    <p>{checked ? "Checked" : "Not Checked"}</p>
   </>
 }
 
